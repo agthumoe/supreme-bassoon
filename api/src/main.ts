c6d6c830551ec8e1de812a84fbbb81393020c349
Aung Thu Moe <aung.thumoe@brainmates.com.au>
2023-01-11 00:48:00 +0630

    chore: integrated prisma and swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Initialize Swagger using `SwaggerModule` class
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Supreme Bassoon API')
+    .setDescription('Core API engine for code test')
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'jwt',
+    )
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
